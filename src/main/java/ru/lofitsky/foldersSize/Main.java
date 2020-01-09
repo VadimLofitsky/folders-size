d9f5187d578eae6ab34dddf7c93b137b9472b86e
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-01-09 10:39:20 +0300

    The application outputs the contents of the predefined path to the browser.

--- src/main/java/ru/lofitsky/foldersSize/Main.java
+++ src/main/java/ru/lofitsky/foldersSize/Main.java
@@ -2,23 +2,11 @@ package ru.lofitsky.foldersSize;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ru.lofitsky.foldersSize.util.FileSizeEntry;
-import ru.lofitsky.foldersSize.util.MyFile;
-
-import java.io.IOException;
 
 @SpringBootApplication
 public class Main {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
-//        String path_D = "d:\\Ленин в ссылке.jpg";
-//        String path_D = "d:\\_Home Media Server";
-        String path_D = "d:\\JavaPrjs";
-        MyFile root_D = new MyFile(path_D, null);
-
-        for (FileSizeEntry entry : root_D.getChildren()) {
-            System.out.println(entry);
-        }
     }
 }
--- src/main/java/ru/lofitsky/foldersSize/controller/Controller.java
+++ src/main/java/ru/lofitsky/foldersSize/controller/Controller.java
@@ -1,13 +1,18 @@
 package ru.lofitsky.foldersSize.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.lofitsky.foldersSize.service.FilesService;
 
 @RestController
 public class Controller {
 
+    @Autowired
+    private FilesService service;
+
     @RequestMapping("/")
     String index() {
-        return "123";
+        return service.getFiles("d:\\JavaPrjs\\git\\bookstoreexample");
     }
 }
