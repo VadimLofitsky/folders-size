fff1f446e6b13d3d2f9669aa1ca91fb3340611fc
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-03-24 02:54:46 +0300

    Open browser page with app interface.

--- src/main/java/ru/lofitsky/foldersSize/SpringApp.java
+++ src/main/java/ru/lofitsky/foldersSize/SpringApp.java
@@ -3,10 +3,14 @@ package ru.lofitsky.foldersSize;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class SpringApp {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         SpringApplication.run(SpringApp.class, args);
+
+        Process process = Runtime.getRuntime().exec("explorer http://localhost:8080/");
     }
 }
