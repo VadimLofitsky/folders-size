2eb2a629f2389232e5a84938643763862e16f9a0
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-03-24 05:06:21 +0300

    Getting shell command for opening browser according to using OS.

--- src/main/java/ru/lofitsky/foldersSize/SpringApp.java
+++ src/main/java/ru/lofitsky/foldersSize/SpringApp.java
@@ -3,14 +3,29 @@ package ru.lofitsky.foldersSize;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.IOException;
-
 @SpringBootApplication
 public class SpringApp {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception {
         SpringApplication.run(SpringApp.class, args);
 
-        Process process = Runtime.getRuntime().exec("explorer http://localhost:8080/");
+        String shellCommand = getShellCommand();
+        String url = "http://localhost:8080/";
+
+        Process process = Runtime.getRuntime().exec(shellCommand + " " + url);
+    }
+
+    private static String getShellCommand() throws Exception {
+        String os = System.getProperty("os.name").toLowerCase();
+
+        if(os.indexOf("windows") >= 0) return "explorer";
+
+        if(os.indexOf("nix") >= 0
+                || os.indexOf("nux") >= 0
+                || os.indexOf("aix") >= 0) return "xdg-open";
+
+        if(os.indexOf("mac") >= 0) return "open";
+
+        throw new Exception("Unsupported operating system");
     }
 }
