fe4a36cc6869ef0c36022aa26350180daf3661dd
VadimLofitsky <54962065+VadimLofitsky@users.noreply.github.com>
2020-03-24 19:29:23 +0300

    Restructuring view...

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -4,34 +4,6 @@ function pageInit() {
     document.body.onclick = onBodyClick;
 }
 
-function exitButtonMouseEnter() {
-    var element = window.event.srcElement;
-    element.classList.remove("far");
-    element.classList.add("fas");
-}
-
-function exitButtonMouseLeave() {
-    var element = window.event.srcElement;
-    element.classList.remove("fas");
-    element.classList.add("far");
-}
-
-function exitButtonClick() {
-    if (confirm("Really quit?")) {
-        var xhr = new XMLHttpRequest();
-        xhr.open("POST", "/exit", true);
-        xhr.send(null);
-
-        document.body.classList.add("waiting");
-        document.querySelector("table#filesShow").classList.add("anim-exiting");
-        window.setTimeout(exitThis, 1000);
-    }
-}
-
-function exitThis() {
-    window.close();
-}
-
 function onBodyClick(clickEvent) {
     var element = window.event.srcElement;
     if (element.tagName != "TD") {
@@ -65,51 +37,14 @@ function getNewTable(newPath) {
         } else {
             var element = document.createElement("html");
             element.innerHTML = xhr.responseText;
-            document.querySelector("table#filesShow").outerHTML = element.querySelector("table#filesShow").outerHTML;
+            element.querySelector("table#filesShow").outerHTML = element.querySelector("table#filesShow").outerHTML;
             waitingMode(false);  // not necessary. Table contents replaced
         }
     }
 }
 
-
 function waitingMode(turnOn) {
     makeBodyWaiting(turnOn);
     balanceScaleAnimate(turnOn);
     logoHide(turnOn);
-}
-
-function makeBodyWaiting(turnOn) {
-    var bodyClassList = document.body.classList;
-
-    if (turnOn) {
-        bodyClassList.add("waiting");
-    } else {
-        bodyClassList.remove("waiting");
-    }
-}
-
-function balanceScaleAnimate(animate) {
-    var els = document.querySelectorAll("table#filesShow td#logo>*[class*='balance-scale']");
-
-    if (animate) {
-        els.forEach(function (el) {
-            el.classList.remove("balance-scale-hidden");
-            el.classList.add("fade-in-out");
-        });
-    } else {
-        els.forEach(function (el) {
-            el.classList.add("balance-scale-hidden");
-            el.classList.remove("fade-in-out");
-        })
-    }
-}
-
-function logoHide(hideLogo) {
-    var logoClassList = document.querySelector(".logo").classList;
-
-    if (hideLogo) {
-        logoClassList.add("hidden");
-    } else {
-        logoClassList.remove("hidden");
-    }
 }
